eba44e228f6abcb329f3232dcc6104553dd1026c
przemkovv <przemkovv@gmail.com>
2018-05-13 03:25:49 +0200

    Fixed plot script for var_ratio

--- scripts/make_plots_iversen_comparison.sh
+++ scripts/make_plots_iversen_comparison.sh
@@ -17,14 +17,12 @@ fi
 
 if [[ $1 -eq 2 ]]; then
   DATA="data/results/var_ratio.json"
-  $PLOT $DATA $OUT_DIR -x2 -y2 --relatives --linear -p P_block --groups="('G0',)" --x_max=1.8 --x_min=0.4 --pairs="(0,1),(2,3),(4,5)," --y_min=50 --y_max=150  -n_50percent_range_similarity $SAVE
-  $PLOT $DATA $OUT_DIR -x2 -y2 --relatives --linear -p P_block --groups="('G0',)" --x_max=1.8 --x_min=0.4 --pairs="(0,1),(2,3),(4,5)," --y_min=98 --y_max=102 -n _2percent_range_similarity $SAVE
-  $PLOT $DATA $OUT_DIR -x2 -y3 --relatives --linear -p P_block --groups="('G0',)" --x_max=1.8 --x_min=0.4 --pairs="(0,2),(2,4),(1,3),(3,5)" --y_min=50 --y_max=150  -n_50percent_range_convergence $SAVE
-  $PLOT $DATA $OUT_DIR -x2 -y3 --relatives --linear -p P_block --groups="('G0',)" --x_max=1.8 --x_min=0.4 --pairs="(0,2),(2,4),(1,3),(3,5)" --y_min=98 --y_max=102 -n _2percent_range_convergence $SAVE
+  $PLOT $DATA $OUT_DIR -x3 -y1 --relatives --linear -p P_block --groups="('G0',)" --x_max=1.8 --x_min=0.4 --pairs="(0,1),(2,3),(4,5)," --y_min=50 --y_max=150  -n_50percent_range_similarity $SAVE
+  $PLOT $DATA $OUT_DIR -x3 -y1 --relatives --linear -p P_block --groups="('G0',)" --x_max=1.8 --x_min=0.4 --pairs="(0,1),(2,3),(4,5)," --y_min=98 --y_max=102 -n _2percent_range_similarity $SAVE
 fi
 if [[ $1 -eq 3 ]]; then
   DATA="data/results/var_ratio.json"
-  # $PLOT $DATA -x1 -y1 --normal -p P_block -i0 --groups="('G0',)" --x_max=1.6 --x_min=0.4 -n_0 $SAVE
+  $PLOT $DATA -x1 -y1 --normal -p P_block -i0 --groups="('G0',)" --x_max=1.6 --x_min=0.4 -n_0 $SAVE
   # $PLOT $DATA -x1 -y1 --normal -p P_block -i1 --groups="('G0',)" --x_max=1.6 --x_min=0.4 -n_1 $SAVE
   $PLOT $DATA -x1 -y1 --normal -p P_block -i2 --groups="('G0',)" --x_max=1.6 --x_min=0.4 -n_2 $SAVE
   # $PLOT $DATA -x1 -y1 --normal -p P_block -i3 --groups="('G0',)" --x_max=1.6 --x_min=0.4 -n_3 $SAVE
