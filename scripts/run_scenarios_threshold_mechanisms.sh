0ce340b7a595cd39e4e551d65aeb56f63b544e17
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2019-04-04 01:47:16 +0200

    Scenarios threshld mechanisms.

--- scripts/run_scenarios_threshold_mechanisms.sh
+++ scripts/run_scenarios_threshold_mechanisms.sh
@@ -1,39 +1,41 @@
 
 set -x
-C=1
+C=3
 
 A_start=0.5
 A_stop=2.0
-A_step=0.2
-Duration=10000
+A_step=0.1
+Duration=800000
 Suffix=
-Path=data/journal/
-OutputDir=data/results/journal/
+Path=data/scenarios/threshold_mechanisms2/
+OutputDir=data/results/threshold_mechanisms3/
 
 CompressionPath=${Path}compression/
-CompressionMods=(t9_3 t9_6 t9_6_3)
+# CompressionMods=(t9_3 t9_6 t9_6_3 t9_int33 t9_int66 t9_int66_int33)
+CompressionMods=(t9_6_3 t9_int66_int33)
+# CompressionMods=(t9_int33 t9_int66 t9_int66_int33)
 OverloadPath=${Path}overload/
 OverloadMods=(im_1.5 im_1.2)
 
 ext=.json
 
-time build/bin/sim -I ${Path}3_1g/ -o 3_1g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
-time build/bin/sim -I ${Path}3g/ -o 3g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
+time build/bin/mutosim -I ${Path}3_1g/ -o 3_1g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
+time build/bin/mutosim -I ${Path}3g/ -o 3g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
 
 for compressionMod in ${CompressionMods[@]}; do
   Suffix=_${compressionMod}
   compressionModPath=${CompressionPath}${compressionMod}${ext}
   echo $compressionModPath
-  time build/bin/sim -I ${Path}3_1g/ -a ${compressionModPath} -o 3_1g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
-  time build/bin/sim -I ${Path}3g/ -a ${compressionModPath} -o 3g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
+  time build/bin/mutosim -I ${Path}3_1g/ -a ${compressionModPath} -o 3_1g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
+  time build/bin/mutosim -I ${Path}3g/ -a ${compressionModPath} -o 3g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
 done
 
 for overloadMod in ${OverloadMods[@]}; do
   Suffix=_${overloadMod}
   overloadModPath=${OverloadPath}${overloadMod}${ext}
   echo $overloadModPath
-  time build/bin/sim -I ${Path}3_1g/ -a ${overloadModPath} -o 3_1g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
-  time build/bin/sim -I ${Path}3g/ -a ${overloadModPath} -o 3g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
+  time build/bin/mutosim -I ${Path}3_1g/ -a ${overloadModPath} -o 3_1g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
+  time build/bin/mutosim -I ${Path}3g/ -a ${overloadModPath} -o 3g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
 done
 
 
@@ -43,7 +45,7 @@ for compressionMod in ${CompressionMods[@]}; do
     Suffix=_${compressionMod}_${overloadMod}
     overloadModPath=${OverloadPath}${overloadMod}${ext}
     echo $overloadModPath
-    time build/bin/sim -I ${Path}3_1g/ -a ${compressionModPath} -a ${overloadModPath} -o 3_1g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
-    time build/bin/sim -I ${Path}3g/ -a ${compressionModPath}  -a ${overloadModPath} -o 3g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
+    time build/bin/mutosim -I ${Path}3_1g/ -a ${compressionModPath} -a ${overloadModPath} -o 3_1g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
+    time build/bin/mutosim -I ${Path}3g/ -a ${compressionModPath}  -a ${overloadModPath} -o 3g${Suffix}.json -c $C --start=$A_start --stop=$A_stop -t $Duration -d ${OutputDir}
   done
 done
